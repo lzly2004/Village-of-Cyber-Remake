68527edd2af3c6d344dae7ad7c56e2141645fac6
lzly2004 <2464277651@qq.com>
2026-06-28 16:15:35 +0800

    P2-4: PlayerStatusRenderer layout dynamicization
    Layout improvement:
    - Extracted hardcoded pixel offsets as named constants:
      - STATUS_START_X = 160 (starting x position)
      - STATUS_ITEM_WIDTH = 64 (item width for layout)
      - DEATH_OFFSET_X = 5 (death icon offset)
      - TEXT_OFFSET_X = 15 (text label offset)
      - ROW_Y_OFFSET = 98 (second row y position)
    - Extracted repeated calculations as local variables:
      - playerSum, halfCount, charIndex
      - charWidth, charHeight, textWidth, textHeight
    - Unified layout coordinate calculation logic
    - Eliminated magic numbers throughout the render method
    
    Benefits:
    - Easier to adjust UI layout in one place
    - Self-documenting constants explain what each value means
    - Reduced code duplication

--- vocr/src/PlayerStatusRenderer.java
+++ vocr/src/PlayerStatusRenderer.java
@@ -6,8 +6,16 @@ import javax.swing.*;
  */
 class PlayerStatusRenderer {
 
+    private static final int STATUS_START_X = 160;
+    private static final int STATUS_ITEM_WIDTH = 64;
+    private static final int DEATH_OFFSET_X = 5;
+    private static final int TEXT_OFFSET_X = 15;
+    private static final int ROW_Y_OFFSET = 98;
+
     static void render(UI ui, GameSceneVoteHandler handler) {
-        for (int i = 1; i <= ui.ctx.getPlayerSum(); i++) {
+        int playerSum = ui.ctx.getPlayerSum();
+        int halfCount = (playerSum + 1) / 2;
+        for (int i = 1; i <= playerSum; i++) {
             StringBuilder xName = new StringBuilder();
             StringBuilder imageName = new StringBuilder();
             if (ui.ctx.getCharacterNumber(i) <= 9) imageName.append("0");
@@ -32,17 +40,17 @@ class PlayerStatusRenderer {
                         ui.claimedRolenum[i][ui.ctx.getGameDay()] = ui.ctx.getClaimedRole(i);
                         ui.skillTargetOrder[i][ui.ctx.getGameDay()] = ui.ctx.getClaimedRoleOrder(i);
                         if (ui.ctx.getClaimedRole(i) != 3) {
-                            if (lastSkillTarget >= ui.ctx.getPlayerSum() + 1) {
+                            if (lastSkillTarget >= playerSum + 1) {
                                 skillTargetName.append("black.png");
-                                ui.skillTargetPeople[i][ui.ctx.getGameDay()] = (lastSkillTarget - ui.ctx.getPlayerSum());
+                                ui.skillTargetPeople[i][ui.ctx.getGameDay()] = (lastSkillTarget - playerSum);
                             } else {
                                 skillTargetName.append("white.png");
                                 ui.skillTargetPeople[i][ui.ctx.getGameDay()] = (lastSkillTarget);
                             }
                         } else {
-                            if (lastSkillTarget >= ui.ctx.getPlayerSum() + 1) {
+                            if (lastSkillTarget >= playerSum + 1) {
                                 skillTargetName.append(".png");
-                                ui.skillTargetPeople[i][ui.ctx.getGameDay()] = (lastSkillTarget - ui.ctx.getPlayerSum());
+                                ui.skillTargetPeople[i][ui.ctx.getGameDay()] = (lastSkillTarget - playerSum);
                             } else {
                                 skillTargetName.append(".png");
                                 ui.skillTargetPeople[i][ui.ctx.getGameDay()] = (lastSkillTarget);
@@ -53,44 +61,45 @@ class PlayerStatusRenderer {
                 }
                 ImageIcon claimedRoleIcon = ui.resources.getImage(claimedRoleIconName);
                 JLabel claimedRoleLabel = new JLabel(claimedRoleIcon);
-                if (i <= (ui.ctx.getPlayerSum() + 1) / 2)
-                    claimedRoleLabel.setBounds(160 + 64 * i, 0,
-                            claimedRoleIcon.getIconWidth(), claimedRoleIcon.getIconHeight());
-                else
-                    claimedRoleLabel.setBounds(160 + 64 * (i - ((ui.ctx.getPlayerSum() + 1) / 2)), 98,
-                            claimedRoleIcon.getIconWidth(), claimedRoleIcon.getIconHeight());
+                int roleX = STATUS_START_X + STATUS_ITEM_WIDTH * (i <= halfCount ? i : (i - halfCount));
+                int roleY = i <= halfCount ? 0 : ROW_Y_OFFSET;
+                claimedRoleLabel.setBounds(roleX, roleY,
+                        claimedRoleIcon.getIconWidth(), claimedRoleIcon.getIconHeight());
                 ui.jPanel.add(claimedRoleLabel);
             }
             if (!xName.isEmpty()) {
                 ImageIcon deathImage = ui.resources.getImage(xName.toString());
                 JLabel deathLabel = new JLabel(deathImage);
-                if (i <= (ui.ctx.getPlayerSum() + 1) / 2)
-                    deathLabel.setBounds(165 + 64 * i, 10,
-                            deathImage.getIconWidth(), deathImage.getIconHeight());
-                else
-                    deathLabel.setBounds(165 + 64 * (i - ((ui.ctx.getPlayerSum() + 1) / 2)), 108,
-                            deathImage.getIconWidth(), deathImage.getIconHeight());
+                int deathX = STATUS_START_X + DEATH_OFFSET_X + STATUS_ITEM_WIDTH * (i <= halfCount ? i : (i - halfCount));
+                int deathY = i <= halfCount ? 10 : (10 + ROW_Y_OFFSET);
+                deathLabel.setBounds(deathX, deathY,
+                        deathImage.getIconWidth(), deathImage.getIconHeight());
                 ui.jPanel.add(deathLabel);
             }
             ImageIcon characterImage = ui.resources.getImage(imageName.toString());
             ImageIcon characterText = ui.resources.getImage(textName);
             JLabel label = new JLabel(characterImage);
             JLabel textLabel;
-            if (i <= (ui.ctx.getPlayerSum() + 1) / 2) {
-                label.setBounds(160 + characterImage.getIconWidth() * i, 0,
-                        characterImage.getIconWidth(), characterImage.getIconHeight());
+            int charWidth = characterImage.getIconWidth();
+            int charHeight = characterImage.getIconHeight();
+            int textWidth = characterText.getIconWidth();
+            int textHeight = characterText.getIconHeight();
+            int charIndex = i <= halfCount ? i : (i - halfCount);
+            if (i <= halfCount) {
+                label.setBounds(STATUS_START_X + charWidth * charIndex, 0,
+                        charWidth, charHeight);
                 textLabel = LabelSimpleFactory.makeLabel(LabelConst.Simple_Label,
-                        175 + characterImage.getIconWidth() * i,
-                        characterImage.getIconHeight() - characterText.getIconHeight() / 2,
-                        characterText.getIconWidth() / 2, characterText.getIconHeight() / 2, characterText);
+                        STATUS_START_X + TEXT_OFFSET_X + charWidth * charIndex,
+                        charHeight - textHeight / 2,
+                        textWidth / 2, textHeight / 2, characterText);
             } else {
-                label.setBounds(160 + characterImage.getIconWidth() * (i - (ui.ctx.getPlayerSum() + 1) / 2),
-                        characterImage.getIconHeight(),
-                        characterImage.getIconWidth(), characterImage.getIconHeight());
+                label.setBounds(STATUS_START_X + charWidth * charIndex,
+                        charHeight,
+                        charWidth, charHeight);
                 textLabel = LabelSimpleFactory.makeLabel(LabelConst.Simple_Label,
-                        175 + characterImage.getIconWidth() * (i - (ui.ctx.getPlayerSum() + 1) / 2),
-                        2 * characterImage.getIconHeight() - characterText.getIconHeight() / 2,
-                        characterText.getIconWidth() / 2, characterText.getIconHeight() / 2, characterText);
+                        STATUS_START_X + TEXT_OFFSET_X + charWidth * charIndex,
+                        2 * charHeight - textHeight / 2,
+                        textWidth / 2, textHeight / 2, characterText);
             }
             ui.jPanel.add(textLabel);
             ui.jPanel.add(label);
